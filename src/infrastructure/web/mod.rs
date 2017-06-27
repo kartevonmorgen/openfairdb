a4d0159fb76ce948a53087a01547302febed3ca7
David Ziegler <david-ziegler@posteo.de>
2017-06-27 09:21:37 +0200

    fix(rating-source): return empty text when no source is given

--- src/infrastructure/web/mod.rs
+++ src/infrastructure/web/mod.rs
@@ -334,7 +334,7 @@ fn get_ratings(db: State<DbPool>, id: String)-> Result<Vec<json::Rating>>{
             user     : usecase::get_user_id_for_rating_id(&triples,&x.id),
             value    : x.value,
             context  : x.context,
-            source   : x.source.unwrap_or("no-source".into()),
+            source   : x.source.unwrap_or("".into()),
             comments : comments.get(&x.id).cloned().unwrap_or_else(|| vec![])
                 .into_iter()
                 .map(|c|json::Comment{
