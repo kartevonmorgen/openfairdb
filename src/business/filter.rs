fc4f7adddce6f45c7be5902a85292c67681d201e
David Ziegler <david-ziegler@posteo.de>
2017-06-27 12:07:32 +0200

    chore(filter): removed unused functions

--- src/business/filter.rs
+++ src/business/filter.rs
@@ -5,10 +5,6 @@ pub trait InBBox {
     fn in_bbox(&self, bb: &[Coordinate]) -> bool;
 }
 
-pub enum Combination {
-    And,
-    Or
-}
 
 impl InBBox for Entry {
     fn in_bbox(&self, bb: &[Coordinate]) -> bool {
@@ -30,51 +26,6 @@ pub fn entries_by_category_ids<'a>(ids: &'a [String]) -> Box<Fn(&&Entry) -> bool
 
 pub fn triple_by_subject<'a>(o_id: ObjectId) -> Box<Fn(&&Triple) -> bool + 'a> {
     Box::new(move |triple| o_id == triple.subject)
-}
-
-pub fn entries_by_tags<'a>(tags: &'a [String], triples: &'a [Triple], combination: &Combination) -> Box<Fn(&&Entry) -> bool + 'a> {
-
-
-    let triples: Vec<(&String, &String)> = triples
-        .into_iter()
-        .filter_map(|x| match *x {
-            Triple {
-                subject   : ObjectId::Entry(ref e_id),
-                predicate : Relation::IsTaggedWith,
-                object    : ObjectId::Tag(ref t_id)
-            } => Some((e_id,t_id)),
-            _ => None
-        })
-        .collect();
-
-    match *combination {
-        Combination::Or => {
-            Box::new(move |entry|
-                tags.iter()
-                    .any(|tag| triples.iter().any(|t| *t.0 == entry.id && t.1 == tag))
-            )
-        },
-        Combination::And => {
-            Box::new(move |entry| {
-                let e_tags : Vec<&String> = triples
-                    .iter()
-                    .filter(|t| *t.0 == entry.id)
-                    .map(|t|t.1)
-                    .collect();
-                    tags.iter().all(|tag|e_tags.iter().any(|t| *t == tag))
-                }
-            )
-        }
-    }
-}
-
-pub fn entries_by_search_text<'a>(text: &'a str) -> Box<Fn(&&Entry) -> bool + 'a> {
-    let words = to_words(text);
-    Box::new(move |e| {
-        words.iter().any(|word| {
-            e.title.to_lowercase().contains(word) || e.description.to_lowercase().contains(word)
-        })
-    })
 }
 
 pub fn entries_by_tags_or_search_text<'a>(text: &'a str, tags: &'a [String], triples: &'a [Triple]) -> Box<Fn(&&Entry) -> bool + 'a> {
@@ -255,31 +206,6 @@ mod tests {
         let x: Vec<&Entry> = entries.iter().cloned().filter(&*filter).collect();
         assert_eq!(x.len(), 1);
         assert_eq!(x[0].id, e2.id);
-    }
-
-    #[test]
-    fn filter_by_tags() {
-        let entries = vec![
-            Entry::build().id("a").finish(),
-            Entry::build().id("b").finish(),
-            Entry::build().id("c").finish(),
-        ];
-        let tags = vec!["csa".into()];
-        let no_triples = vec![];
-        let x: Vec<&Entry> = entries.iter().filter(&*entries_by_tags(&tags,&no_triples, &Combination::Or)).collect();
-        assert_eq!(x.len(), 0);
-        let triples = vec![
-            Triple{ subject: ObjectId::Entry("b".into()), predicate: Relation::IsTaggedWith, object: ObjectId::Tag("csa".into())},
-            Triple{ subject: ObjectId::Entry("c".into()), predicate: Relation::IsTaggedWith, object: ObjectId::Tag("foo".into())}
-        ];
-        let x: Vec<&Entry> = entries.iter().filter(&*entries_by_tags(&tags,&triples, &Combination::Or)).collect();
-        assert_eq!(x.len(), 1);
-        assert_eq!(x[0].id,"b");
-        let tags = vec!["csa".into(),"foo".into()];
-        let x: Vec<&Entry> = entries.iter().filter(&*entries_by_tags(&tags,&triples, &Combination::Or)).collect();
-        assert_eq!(x.len(), 2);
-        let x: Vec<&Entry> = entries.iter().filter(&*entries_by_tags(&tags,&triples, &Combination::And)).collect();
-        assert_eq!(x.len(), 0);
     }
 
      #[test]
