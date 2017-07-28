405f9a39b7a6d9eca35fe77f0ef59261a0dc8f46
David Ziegler <david-ziegler@posteo.de>
2017-07-28 19:16:31 +0200

    fix(notificationEmail): small formatting change in entry notification email

--- src/adapters/user_communication.rs
+++ src/adapters/user_communication.rs
@@ -2,7 +2,7 @@ use business::usecase::{NewEntry, UpdateEntry};
 use entities::Entry;
 
 pub fn email_confirmation_email(u_id: &str) -> String {
-    format!("Na du Weltverbesserer*,\nwir freuen uns dass du bei der Karte von Morgen mit dabei bist!\nBitte bestätige deine Email-Adresse hier:\nhttps://prototyp.kartevonmorgen.org/#/?confirm_email={}.\n\neuphorische Grüße\ndas Karte von Morgen-Team", 
+    format!("Na du Weltverbesserer*,\nwir freuen uns dass du bei der Karte von Morgen mit dabei bist!\nBitte bestätige deine Email-Adresse hier:\nhttps://prototyp.kartevonmorgen.org/#/?confirm_email={}.\neuphorische Grüße\ndas Karte von Morgen-Team",
     u_id)
 }
 
@@ -52,16 +52,22 @@ pub fn changed_entry_email(e: &UpdateEntry, categories: Vec<String>) -> String {
     entry_email(&entry, categories, &e.tags, intro_sentence)
 }
 
-fn entry_email(e: &Entry, categories: Vec<String>, tags: &Vec<String>, intro_sentence: &str) -> String{
+pub fn entry_email(e: &Entry, categories: Vec<String>, tags: &Vec<String>, intro_sentence: &str) -> String{
     let category = if categories.len() > 0 { categories[0].clone() } else { "".to_string() };
+    let address = vec![
+        e.street.clone().unwrap_or("".into()),
+        vec![e.zip.clone().unwrap_or("".into()),
+            e.city.clone().unwrap_or("".into())].join(" "),
+        e.country.clone().unwrap_or("".into())]
+        .join(", ");
 
     format!(
 "Hallo,
 {introSentence}:\n
-    {title} ({category})
+{title} ({category})
 {description}\n
     Tags: {tags}
-    Adresse: {street}, {zip} {city}, {country}
+    Adresse: {address}
     Webseite: {homepage}
     Email-Adresse: {email}
     Telefon: {telephone}\n
@@ -74,10 +80,7 @@ das Karte von Morgen-Team",
         title = &e.title, 
         id = &e.id,
         description= &e.description,
-        street = e.street.clone().unwrap_or("".into()),
-        zip = e.zip.clone().unwrap_or("".into()),
-        city = e.city.clone().unwrap_or("".into()),
-        country = e.country.clone().unwrap_or("".into()),
+        address = address,
         email = e.email.clone().unwrap_or("".into()),
         telephone = e.telephone.clone().unwrap_or("".into()),
         homepage = e.homepage.clone().unwrap_or("".into()),
