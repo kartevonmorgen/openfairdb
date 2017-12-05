6effe6c6fa601c31798558a4cfec43ed22629339
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-12-05 14:54:29 +0100

    new(db): tidy up after csv import

--- scripts/import_into_sqlite.sql
+++ scripts/import_into_sqlite.sql
@@ -9,3 +9,19 @@
 .import bbox_subscriptions.csv bbox_subscriptions
 .import entry_category_relations.csv entry_category_relations
 .import entry_tag_relations.csv  entry_tag_relations
+
+UPDATE entries SET street    = NULL WHERE street    = '';
+UPDATE entries SET zip       = NULL WHERE zip       = '';
+UPDATE entries SET city      = NULL WHERE city      = '';
+UPDATE entries SET country   = NULL WHERE country   = '';
+UPDATE entries SET email     = NULL WHERE email     = '';
+UPDATE entries SET telephone = NULL WHERE telephone = '';
+UPDATE entries SET homepage  = NULL WHERE homepage  = '';
+UPDATE entries SET license   = NULL WHERE license   = '';
+
+UPDATE ratings SET source = NULL WHERE source = '';
+
+DELETE FROM entry_tag_relations WHERE tag_id = '';
+DELETE FROM entry_tag_relations WHERE entry_id = '';
+
+DELETE FROM tags WHERE id = '';
