b0fd31f660e0a04a0c12a733dc3832aebfe4323b
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-12-05 10:40:38 +0100

    new(db): add csv import script

---
+++ scripts/import_into_sqlite.sql
@@ -0,0 +1,11 @@
+.mode csv
+.import entries.csv entries
+.import old_entries.csv entries
+.import tags.csv tags
+.import categories.csv categories
+.import ratings.csv ratings
+.import comments.csv comments
+.import users.csv users
+.import bbox_subscriptions.csv bbox_subscriptions
+.import entry_category_relations.csv entry_category_relations
+.import entry_tag_relations.csv  entry_tag_relations
