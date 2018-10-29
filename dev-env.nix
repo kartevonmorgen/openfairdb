2e81e7a768d375845d72df440255288737d17e35
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-10-29 13:12:14 +0100

    chore(dev): update nix development environment

--- dev-env.nix
+++ dev-env.nix
@@ -1,18 +1,20 @@
 let
-  pkgs    = import <nixpkgs> {};
-  stdenv  = pkgs.stdenv;
-  lib     = pkgs.lib;
-
-in rec {
-  devEnv = stdenv.mkDerivation rec {
-    name = "rust-ofdb-dev-env";
-    src = ./.;
-    buildInputs = with pkgs; [
-      git
-      rustc
-      pkgconfig
-      openssl
-      cargo
-    ];
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = pkgs.rustChannelOf {
+     date = "2018-10-29";
+     channel = "nightly";
   };
+in
+  with pkgs;
+  stdenv.mkDerivation {
+    name = "rust-ofdb-dev-env";
+    buildInputs = [
+      rustChannel.rust
+      cmake
+      pkgconfig
+    ];
+    SQLITE3_DIR = "${sqlite.dev}";
+    SQLITE3_LIB_DIR = "${sqlite.out}/lib";
+    SQLITE3_INCLUDE_LIB_DIR = "${sqlite.out}/include";
 }
