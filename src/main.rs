810bfc1ffdd040ce124cdca483a48dd60785721f
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-11-18 20:05:33 +0100

    remove unnecessary 'extern crate' statementes

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,18 @@
 #![plugin(rocket_codegen)]
 #![allow(proc_macro_derive_resolution_fallback)]
 
-extern crate chrono;
-
-extern crate clap;
-
-extern crate csv;
-
 #[macro_use]
 extern crate diesel;
-
 #[macro_use]
 extern crate diesel_migrations;
-
-extern crate dotenv;
-
-extern crate env_logger;
-
-extern crate fast_chemail;
-
 #[macro_use]
 extern crate lazy_static;
-
 #[macro_use]
 extern crate log;
-
-extern crate pwhash;
-
 #[macro_use]
 extern crate quick_error;
-
-extern crate quoted_printable;
-
-extern crate regex;
-
-extern crate rocket;
-
-extern crate rocket_contrib;
-
 #[macro_use]
 extern crate serde;
-
-extern crate serde_json;
-
-extern crate toml;
-
-extern crate url;
-
-extern crate uuid;
-
 #[cfg(test)]
 extern crate test;
 
