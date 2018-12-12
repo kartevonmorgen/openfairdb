f92bb6cdec0bf35c303096b8cb89b5957e87deed
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-12-12 17:02:55 +0100

    update dependencies

--- dev-env.nix
+++ dev-env.nix
@@ -2,7 +2,7 @@ let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   rustChannel = pkgs.rustChannelOf {
-     date = "2018-12-01";
+     date = "2018-12-08";
      channel = "nightly";
   };
 in
