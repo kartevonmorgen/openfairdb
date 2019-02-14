9d599bd2c13f2e7ae11d208447daa221646dc4c8
Uwe Klotz <uwe.klotz@slowtec.de>
2019-02-14 19:10:54 +0100

    Fix formatting for geocoding requests

--- src/ports/web/api/events.rs
+++ src/ports/web/api/events.rs
@@ -1,5 +1,6 @@
 use super::{super::guards::Bearer, *};
 use geocoding::Opencage;
+use itertools::Itertools;
 use rocket::{
     http::{RawStr, Status},
     request::{FromQuery, Query},
@@ -17,14 +18,8 @@ lazy_static! {
 }
 
 fn to_addr_string(e: &usecases::NewEvent) -> String {
-    let x = e.clone();
-    format!(
-        "{},{},{},{}",
-        x.country.unwrap_or("".into()),
-        x.city.unwrap_or("".into()),
-        x.zip.unwrap_or("".into()),
-        x.street.unwrap_or("".into())
-    )
+    let addr_parts = [&e.street, &e.zip, &e.city, &e.country];
+    addr_parts.into_iter().filter_map(|x| x.as_ref()).join(",")
 }
 
 //TODO: use a trait for the geocoding API and test it with a mock
@@ -237,7 +232,6 @@ pub fn delete_event_with_token(mut db: DbConn, token: Bearer, id: &RawStr) -> Re
 #[cfg(test)]
 mod tests {
     use super::super::tests::prelude::*;
-    use crate::core::entities::*;
     use rocket::http::Header;
 
     mod create {
@@ -1181,6 +1175,38 @@ mod tests {
             assert_eq!(res.status(), Status::Ok);
             assert_eq!(db.get().unwrap().all_events().unwrap().len(), 1);
         }
+    }
+
+    use super::*;
+
+    #[test]
+    fn new_event_to_addr_string_partial() {
+        let mut e = usecases::NewEvent {
+            title        : "foo".into(),
+            description  : Some("bar".into()),
+            start        : 9999,
+            end          : None,
+            lat          : None,
+            lng          : None,
+            street       : Some("A street".into()),
+            zip          : None,
+            city         : Some("A city".into()),
+            country      : None,
+            email        : None,
+            telephone    : None,
+            homepage     : None,
+            tags         : None,
+            created_by   : Some("fooo@bar.tld".into()),
+            token        : None,
+            registration : None,
+            organizer    : None,
+        };
+        assert_eq!("A street,A city", to_addr_string(&e));
+        e.country = Some("A country".into());
+        assert_eq!("A street,A city,A country", to_addr_string(&e));
+        e.street = None;
+        e.zip = Some("1234".into());
+        assert_eq!("1234,A city,A country", to_addr_string(&e));
     }
 
 }
