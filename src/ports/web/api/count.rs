a2a0822ab8ef7896a2a31898566842af994e5ad1
Uwe Klotz <uwe.klotz@slowtec.de>
2019-02-22 11:43:52 +0100

    Add TODO comments

--- src/ports/web/api/count.rs
+++ src/ports/web/api/count.rs
@@ -2,11 +2,13 @@ use super::*;
 
 #[get("/count/entries")]
 pub fn get_count_entries(db: DbConn) -> Result<usize> {
+    // TODO: Replace with count query
     let entries = db.all_entries()?;
     Ok(Json(entries.len()))
 }
 
 #[get("/count/tags")]
 pub fn get_count_tags(db: DbConn) -> Result<usize> {
+    // TODO: Replace with count query
     Ok(Json(db.all_tags()?.len()))
 }
