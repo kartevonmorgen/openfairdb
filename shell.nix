ba2c831ad674e0553576fa51d35296c8927148f0
Markus Kohlhase <markus.kohlhase@slowtec.de>
2019-10-25 13:50:26 +0200

    Update rust toolchain to 2019-10-24

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   rustChannel = pkgs.rustChannelOf {
-     date = "2019-06-13";
+     date = "2019-10-24";
      channel = "nightly";
   };
 in
