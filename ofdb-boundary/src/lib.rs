11b86951ae2cec28b2eb09f22e4ed635185fd7a6
Magnus Herold <magnus.herold@gmail.com>
2020-08-11 13:02:46 +0200

    Add PlaceHistory struct and all it depends on to boundary

--- ofdb-boundary/src/lib.rs
+++ ofdb-boundary/src/lib.rs
@@ -553,3 +553,331 @@ impl From<ClearanceForPlace> for e::clearance::ClearanceForPlace {
 pub struct ResultCount {
     pub count: u64,
 }
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug, PartialEq))]
+pub struct LatLonDegrees(f64, f64);
+
+impl From<e::geo::MapPoint> for LatLonDegrees {
+    fn from(from: e::geo::MapPoint) -> Self {
+        Self(from.lat().to_deg(), from.lng().to_deg())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug, Default, PartialEq, Eq))]
+pub struct Address {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub street: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub zip: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub city: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub country: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub state: Option<String>,
+}
+
+impl Address {
+    pub fn is_empty(&self) -> bool {
+        self.street.is_none()
+            && self.zip.is_none()
+            && self.city.is_none()
+            && self.country.is_none()
+            && self.state.is_none()
+    }
+}
+
+impl From<e::address::Address> for Address {
+    fn from(from: e::address::Address) -> Self {
+        let e::address::Address {
+            street,
+            zip,
+            city,
+            country,
+            state,
+        } = from;
+        Self {
+            street,
+            zip,
+            city,
+            country,
+            state,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug, PartialEq))]
+pub struct Location {
+    #[serde(rename = "deg")]
+    pub latlon: LatLonDegrees,
+
+    #[serde(
+        rename = "adr",
+        skip_serializing_if = "Address::is_empty",
+        default = "Default::default"
+    )]
+    pub address: Address,
+}
+
+impl From<e::location::Location> for Location {
+    fn from(from: e::location::Location) -> Self {
+        let e::location::Location { pos, address } = from;
+        Self {
+            latlon: pos.into(),
+            address: address.map(Into::into).unwrap_or_default(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug, Default, PartialEq, Eq))]
+pub struct Contact {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub email: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub phone: Option<String>,
+}
+
+impl Contact {
+    pub fn is_empty(&self) -> bool {
+        self.email.is_none() && self.phone.is_none()
+    }
+}
+
+impl From<e::contact::Contact> for Contact {
+    fn from(from: e::contact::Contact) -> Self {
+        let e::contact::Contact { email, phone } = from;
+        Self {
+            email: email.map(Into::into),
+            phone,
+        }
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Links {
+    #[serde(rename = "www", skip_serializing_if = "Option::is_none")]
+    pub homepage: Option<Url>,
+
+    #[serde(rename = "img", skip_serializing_if = "Option::is_none")]
+    pub image: Option<Url>,
+
+    #[serde(rename = "img_href", skip_serializing_if = "Option::is_none")]
+    pub image_href: Option<Url>,
+}
+
+impl Links {
+    pub fn is_empty(&self) -> bool {
+        self.homepage.is_none() && self.image.is_none() && self.image_href.is_none()
+    }
+}
+
+impl From<e::links::Links> for Links {
+    fn from(from: e::links::Links) -> Self {
+        let e::links::Links {
+            homepage,
+            image,
+            image_href,
+        } = from;
+        Self {
+            homepage,
+            image,
+            image_href,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug, PartialEq, Eq))]
+pub struct Activity {
+    pub at: i64,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub by: Option<String>,
+}
+
+impl From<e::activity::Activity> for Activity {
+    fn from(from: e::activity::Activity) -> Self {
+        let e::activity::Activity { at, by } = from;
+        Self {
+            at: at.into_inner(),
+            by: by.map(Into::into),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug))]
+pub struct PlaceRoot {
+    pub id: String,
+
+    #[serde(rename = "lic")]
+    pub license: String,
+}
+
+impl From<e::place::PlaceRoot> for PlaceRoot {
+    fn from(from: e::place::PlaceRoot) -> Self {
+        let e::place::PlaceRoot { id, license } = from;
+        Self {
+            id: id.into(),
+            license,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug))]
+pub struct PlaceRevision {
+    #[serde(rename = "rev")]
+    pub revision: u64,
+
+    pub created: Activity,
+
+    #[serde(rename = "tit")]
+    pub title: String,
+
+    #[serde(rename = "dsc")]
+    pub description: String,
+
+    #[serde(rename = "loc")]
+    pub location: Location,
+
+    #[serde(
+        rename = "cnt",
+        skip_serializing_if = "Contact::is_empty",
+        default = "Default::default"
+    )]
+    pub contact: Contact,
+
+    #[serde(rename = "hrs", skip_serializing_if = "Option::is_none")]
+    pub opening_hours: Option<String>,
+
+    #[serde(
+        rename = "lnk",
+        skip_serializing_if = "Links::is_empty",
+        default = "Default::default"
+    )]
+    pub links: Links,
+
+    #[serde(
+        rename = "tag",
+        skip_serializing_if = "Vec::is_empty",
+        default = "Default::default"
+    )]
+    pub tags: Vec<String>,
+}
+
+impl From<e::place::PlaceRevision> for PlaceRevision {
+    fn from(from: e::place::PlaceRevision) -> Self {
+        let e::place::PlaceRevision {
+            revision,
+            created,
+            title,
+            description,
+            location,
+            contact,
+            opening_hours,
+            links,
+            tags,
+        } = from;
+        Self {
+            revision: revision.into(),
+            created: created.into(),
+            title,
+            description,
+            location: location.into(),
+            contact: contact.map(Into::into).unwrap_or_default(),
+            opening_hours: opening_hours.map(Into::into),
+            links: links.map(Into::into).unwrap_or_default(),
+            tags,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug))]
+pub struct PlaceHistory {
+    pub place: PlaceRoot,
+    pub revisions: Vec<(PlaceRevision, Vec<ReviewStatusLog>)>,
+}
+
+impl From<e::place::PlaceHistory> for PlaceHistory {
+    fn from(from: e::place::PlaceHistory) -> Self {
+        let e::place::PlaceHistory { place, revisions } = from;
+        Self {
+            place: place.into(),
+            revisions: revisions
+                .into_iter()
+                .map(|(place_revision, reviews)| {
+                    (
+                        place_revision.into(),
+                        reviews.into_iter().map(Into::into).collect(),
+                    )
+                })
+                .collect(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug))]
+pub struct ActivityLog {
+    pub at: i64,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub by: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ctx: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub comment: Option<String>,
+}
+
+impl From<e::activity::ActivityLog> for ActivityLog {
+    fn from(from: e::activity::ActivityLog) -> Self {
+        let e::activity::ActivityLog {
+            activity: e::activity::Activity { at, by },
+            context: ctx,
+            comment,
+        } = from;
+        Self {
+            at: at.into_inner(),
+            by: by.map(Into::into),
+            ctx,
+            comment,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg_attr(feature = "extra-derive", derive(Debug))]
+pub struct ReviewStatusLog {
+    pub rev: u64,
+    pub act: ActivityLog,
+    pub status: ReviewStatus,
+}
+
+impl From<e::review::ReviewStatusLog> for ReviewStatusLog {
+    fn from(from: e::review::ReviewStatusLog) -> Self {
+        let e::review::ReviewStatusLog {
+            revision,
+            activity,
+            status,
+        } = from;
+        Self {
+            rev: revision.into(),
+            act: activity.into(),
+            status: status.into(),
+        }
+    }
+}
+
