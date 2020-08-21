c4e103cda34c4ca362fbc4b1a4268376ed1f03c7
Markus Kohlhase <markus.kohlhase@slowtec.de>
2020-08-21 12:50:28 +0200

    Update rust in shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   rustChannel = pkgs.rustChannelOf {
-     date = "2020-06-08";
+     date = "2020-08-21";
      channel = "nightly";
   };
   rust = (rustChannel.rust.override {
