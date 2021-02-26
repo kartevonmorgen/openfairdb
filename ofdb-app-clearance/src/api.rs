f7043e64cdd5f2df515ba09523c4d5b14dd967a5
Markus Kohlhase <markus.kohlhase@slowtec.de>
2021-02-26 14:55:26 +0100

    Clearance center: lazy load place histories

--- ofdb-app-clearance/src/api.rs
+++ ofdb-app-clearance/src/api.rs
@@ -6,23 +6,21 @@ pub const API_ROOT: &str = "/api";
 #[derive(Clone, Debug)]
 pub struct PlaceClearance {
     pub pending: PendingClearanceForPlace,
-    pub history: PlaceHistory,
-    pub expanded: bool,
+    pub history: Option<PlaceHistory>,
 }
 
 impl PlaceClearance {
     pub fn last_cleared_rev_nr(&self) -> Option<u64> {
         self.pending.last_cleared_revision
     }
-    pub fn current_rev_nr(&self) -> u64 {
-        self.current_rev().revision.into()
-    }
-    pub fn current_rev(&self) -> &PlaceRevision {
-        &self.history.revisions[0].0
+    pub fn current_rev(&self) -> Option<&PlaceRevision> {
+        self.history
+            .as_ref()
+            .and_then(|h| h.revisions.iter().nth(0).map(|(rev, _)| rev))
     }
     pub fn last_cleared_rev(&self) -> Option<&PlaceRevision> {
         let nr = self.last_cleared_rev_nr()?;
-        let rev = self.history.revisions.iter().find(|&x| {
+        let rev = self.history.as_ref()?.revisions.iter().find(|&x| {
             let r: u64 = x.0.revision.into();
             r == nr
         })?;
@@ -32,7 +30,11 @@ impl PlaceClearance {
         if let Some(rev) = self.last_cleared_rev() {
             &rev.title
         } else {
-            &self.current_rev().title
+            if let Some(r) = self.current_rev() {
+                &r.title
+            } else {
+                &self.pending.place_id
+            }
         }
     }
 }
