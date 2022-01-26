e2ce2ad5d4f9fdcf4c8870032af7c9795993654c
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-01-26 17:02:50 +0100

    Add some debug logs to review endpoint

--- src/ports/web/api/places.rs
+++ src/ports/web/api/places.rs
@@ -48,3 +48,56 @@ pub fn update_pending_clearances(
         count: count as u64,
     }))
 }
+
+#[post("/places/<ids>/review", data = "<review>")]
+pub fn post_review(
+    auth: Auth,
+    db: sqlite::Connections,
+    mut search_engine: tantivy::SearchEngine,
+    ids: String,
+    review: JsonResult<json::Review>,
+) -> Result<()> {
+    let ids = util::split_ids(&ids);
+    if ids.is_empty() {
+        log::debug!("No places to review");
+        return Err(Error::Parameter(ParameterError::EmptyIdList).into());
+    }
+    let reviewer_email = {
+        let db = db.shared()?;
+        // Only scouts and admins are entitled to review places
+        auth.user_with_min_role(&*db, Role::Scout)
+            .map_err(|err| {
+                log::debug!("Unauthorized user: {}", err);
+                err
+            })?
+            .email
+    };
+    let json::Review { status, comment } = review
+        .map_err(|err| {
+            log::debug!("Invalid review: {:?}", err);
+            err
+        })?
+        .into_inner();
+    // TODO: Record context information
+    let context = None;
+    let review = usecases::Review {
+        context,
+        reviewer_email: reviewer_email.into(),
+        status: status.into(),
+        comment,
+    };
+    let update_count =
+        flows::review_places(&db, &mut search_engine, &ids, review).map_err(|err| {
+            log::debug!("Unable to review places: {}", err);
+            err
+        })?;
+    if update_count < ids.len() {
+        log::warn!(
+            "Applied review to only {} of {} place(s): {:?}",
+            update_count,
+            ids.len(),
+            ids
+        );
+    }
+    Ok(Json(()))
+}
