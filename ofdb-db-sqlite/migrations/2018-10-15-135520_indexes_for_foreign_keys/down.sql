5aa80aba0042875589e770de93633dcbca9b3a49
Uwe Klotz <uwe.klotz@slowtec.de>
2022-09-02 00:16:01 +0200

    Delete useless down.sql migrations

--- ofdb-db-sqlite/migrations/2018-10-15-135520_indexes_for_foreign_keys/down.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX entry_category_relations_fk_category_id;
-
-DROP INDEX entry_tag_relations_fk_tag_id;
-
-DROP INDEX comments_fk_rating_id;
-
-DROP INDEX bbox_subscriptions_fk_username;
--- ofdb-db-sqlite/migrations/2019-03-05_archived_timestamp/down.sql
+++
@@ -1,2 +0,0 @@
--- This file should undo anything in `up.sql`
--- Removing columns from a table is not supported by SQLite
