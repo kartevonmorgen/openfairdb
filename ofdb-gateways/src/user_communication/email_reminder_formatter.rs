4332f6c3199b6a21fa3372dbb005fd7fc61e5cee
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-11-22 15:58:37 +0100

    Use formatted date in reminder emails

--- ofdb-gateways/src/user_communication/email_reminder_formatter.rs
+++ ofdb-gateways/src/user_communication/email_reminder_formatter.rs
@@ -1,9 +1,12 @@
 use askama::Template;
 use ofdb_core::usecases::{EmailReminderFormatter, Reminder};
 use ofdb_entities::{email::EmailContent, place::Place};
+use time::{format_description::FormatItem, macros::format_description};
 
 #[derive(Default)]
 pub struct ReminderFormatter; // TODO: support different languages
+
+const DATE_FORMAT_DE: &[FormatItem] = format_description!("[day].[month].[year]");
 
 impl EmailReminderFormatter for ReminderFormatter {
     fn format_email(&self, r: &Reminder) -> EmailContent {
@@ -13,10 +16,12 @@ impl EmailReminderFormatter for ReminderFormatter {
             description,
             ..
         } = &r.place;
+        let last_change = &r.last_change.format(DATE_FORMAT_DE); // TODO: support i18n
         let subject = EmailReminderScoutsSubjectTemplate { title }
             .render()
             .unwrap();
         let body = EmailReminderScoutsBodyTemplate {
+            last_change,
             title,
             description,
             id: id.as_str(),
@@ -36,6 +41,7 @@ struct EmailReminderScoutsSubjectTemplate<'a> {
 #[derive(Template)]
 #[template(path = "email_reminder_scouts/body_DE.txt")]
 struct EmailReminderScoutsBodyTemplate<'a> {
+    last_change: &'a str,
     title: &'a str,
     description: &'a str,
     id: &'a str,
