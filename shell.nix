2e6f7a6ac43bde6a2833697b3c830e786b201ad5
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-12-02 15:09:06 +0100

    Pin Rust to 1.73

--- shell.nix
+++ shell.nix
@@ -2,18 +2,9 @@ let
   fenix_overlay = import "${fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"}/overlay.nix";
   pkgs = import <nixpkgs> { overlays = [ fenix_overlay ]; };
   fenix = pkgs.fenix;
-  rust_stable = fenix.stable;
-  rust_targets = with fenix.targets; [
-    wasm32-unknown-unknown.stable.rust-std
-    x86_64-unknown-linux-musl.stable.rust-std
-  ];
-  rust = with rust_stable; (fenix.combine [
-    rustc
-    cargo
-    clippy
-    rustfmt
-    rust_targets
-  ]);
+  rust = fenix.fromToolchainFile {
+    file = ./rust-toolchain.toml;
+  };
   sass = with pkgs; stdenv.mkDerivation rec {
     name = "dart-sass-${version}";
     version = "1.63.6";
